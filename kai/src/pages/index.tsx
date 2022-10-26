422bc2fc3dce4e1359800e6411ccf60d6a5c67f0
AigaKai100 <116280330+AigaKai100@users.noreply.github.com>
2022-10-26 17:59:28 +0900

    kai

--- kai/src/pages/index.tsx
+++ kai/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,51 +5,48 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+function createLabel(
+  Label_Text : string,
+){
+  return{Label_Text};
+}
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  calories: string,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { name, calories};
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('団体名', '東北大学  FROM THE EARTH'),
+  createData('設立', '2011年XX月XX日'),
+  createData('代表', '長内 海渡'),
+  createData('現役メンバー部員', '46人'),
 ];
+
+const Label_Text = [
+  createLabel('団体紹介'),
+]
 
 export default function BasicTable() {
   return (
+    
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+      <h1>.Label_Text</h1>
+      <Table sx={{ minWidth: 100 }} aria-label="">
+        <TableBody sx={{ '&:last-child td ,&:last-child th': { border: 3 } }}>
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              
             >
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="left">{row.calories}</TableCell>
             </TableRow>
           ))}
         </TableBody>
